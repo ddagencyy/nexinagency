d4e74b42b178e3074bbfd0fd2ab615aa827be722
unknown <buissnessworld4@gmail.com>
2026-01-25 22:19:37 -0800

    Delete loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoadingScreen } from "@/components/loading-screen";
+// import { LoadingScreen } from "@/components/loading-screen";
 import Index from "./pages/Index";
 import AllProjects from "./pages/AllProjects";
 import NotFound from "./pages/NotFound";
